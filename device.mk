91379c7c7bd201342b37b63cbe65186aa08897d6
Sean Hoyt <seanhoyt963@gmail.com>
2017-02-05 21:20:50 -0500

    Do not expand local_path for overlays
    Change-Id: I63a529a5f4e9fde8a1723d181f7faad61d0872f0
    (cherry picked from commit 3405f0ed3bba994f3268769a32747b344d41d55a)

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := device/samsung/klimtwifi
 
 PRODUCT_CHARACTERISTICS := tablet
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/klimtwifi/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal large xlarge
