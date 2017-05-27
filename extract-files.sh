a895ca0c7256dee1604afdc03d32bbeeeb97115d
Patrik Stromdahl <6065kc@gmail.com>
2017-05-27 23:40:46 +0200

    Changes to files in device/samsung/klimtwifi
    Added execute permissions to extract-files.sh and setup-makefiles.sh
    
    Corrected an error in the proprietary-files.txt file. (Wrong path to the
    LVVEFS_Rx_Configuration.txt file).
    
    Small modification to the extract-files.sh script to handle the fact that
    the /system/bin/gpsd file requires root privileges to be extracted from
    the device via adb.
    
    Change-Id: Ia22452aea0fa71dd828d9b04254a084e80ea47ab

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,20 @@ DEVICE=klimtwifi
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
+set -e
+
+if [ -z "$STOCK_ROM_DIR" ]; then
+    if [ "$(adb root | grep -c 'adbd')" -eq 1  ]; then
+	echo "adbd root: OK!"
+	adb wait-for-device
+    else
+	echo "/system/bin/gpsd requires root privileges to be extracted"
+	echo "root access is disabled by system setting - enable in settings -> development options"
+	adb wait-for-device
+	exit
+    fi
+fi
+
 for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
     OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
     FILE=${PARSING_ARRAY[0]}
