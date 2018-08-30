f6d6939c58980fad6b010fdb6ed3420389bf2b14
Vasyl Gello <vasek.gello@gmail.com>
2018-08-30 23:39:51 +0300

    klimtwifi: Import common vendor scripts
    Import common extract-files.sh and setup-makefiles.sh
    from klte.
    
    Also add functionality to wait for "adb root",
    to ensure extraction of system/bin/gpsd.
    
    Change-Id: Icd43892faaa84073602dc2ee04a45e3ad9741b28

--- extract-files.sh
+++ extract-files.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-
-VENDOR=samsung
-DEVICE=klimtwifi
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
+
+export DEVICE=klimtwifi
+export DEVICE_COMMON=exynos5420-common
+export VENDOR=samsung
+export INITIAL_COPYRIGHT_YEAR=2016
 
 if [ -z "$STOCK_ROM_DIR" ]; then
     if [ "$(adb root | grep -c 'adbd')" -eq 1  ]; then
@@ -20,30 +34,4 @@ if [ -z "$STOCK_ROM_DIR" ]; then
     fi
 fi
 
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-    FILE=${PARSING_ARRAY[0]}
-    DEST=${PARSING_ARRAY[1]}
-    if [ -z $DEST ]
-    then
-        DEST=$FILE
-    fi
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-
-    if [ -z "$STOCK_ROM_DIR" ]; then
-        adb pull /system/$FILE $BASE/$DEST
-    else
-        cp $STOCK_ROM_DIR/$FILE $BASE/$DEST
-    fi
-
-    # if file does not exist try destination
-    if [ "$?" != "0" ]
-    then
-        adb pull /system/$DEST $BASE/$DEST
-    fi
-done
-
-./setup-makefiles.sh
+./../$DEVICE_COMMON/extract-files.sh $@
